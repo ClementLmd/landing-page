9c7083e98bb2be88c6059f405757fb5ab083c2ec
clement <clement.laumond@gmail.com>
2024-06-21 12:58:39 +0200

    modif favicon

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
