f99dcbe679d7c9e1bc59197dad0a6b7a7fa044ca
clement lmd <clement.laumond@gmail.com>
2025-12-18 21:34:51 +0100

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/next";
 import Footer from "../components/Footer";
 import Navbar from "../components/NavBar";
 import "../styles/globals.css";
@@ -15,6 +16,7 @@ export default function RootLayout({
         <div className="layout">
           <Navbar />
           <div className="content">{children}</div>
+          <Analytics />
           <Footer />
         </div>
       </body>
